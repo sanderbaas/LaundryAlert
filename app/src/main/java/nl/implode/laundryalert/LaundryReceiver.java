cabf06190e0d5604f207d21e9c041b7b221ec87c
Sander Baas <sander@implode.nl>
2018-05-07 17:23:00 +0200

    add notification channel

--- app/src/main/java/nl/implode/laundryalert/LaundryReceiver.java
+++ app/src/main/java/nl/implode/laundryalert/LaundryReceiver.java
@@ -17,43 +17,45 @@ import android.widget.Toast;
 import static android.app.Notification.EXTRA_NOTIFICATION_ID;
 
 public class LaundryReceiver extends BroadcastReceiver {
-    public static final String CHANNEL_ID = "nl.implode.laundryalert.ANDROID";
-    public static final String CHANNEL_NAME = "ANDROID CHANNEL";
+    public static final String CHANNEL_ID = "nl.implode.laundryalert";
+    public static final String CHANNEL_NAME = "Laundry Alert Channel";
+    public static final String CHANNEL_DESCRIPTION = "Keep in touch with the laundry machine.";
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
         Log.d("Laundry", "I'm running");
-        //Toast.makeText(context, "I'm running", Toast.LENGTH_SHORT).show();
         checkAndAlert(context, intent);
     }
 
     public void checkAndAlert(Context context, Intent intent) {
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+        createNotificationChannel(context);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_notifications_black_24dp)
                 .setContentTitle("Hi y'all")
                 .setContentText("Daar ben ik dan!")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-        Integer notificationId = 242424;
+        Integer notificationId = 242424; // should be generated and saved
         notificationManager.notify(notificationId, mBuilder.build());
 
     }
 
-    /*private void createNotificationChannel() {
+    private void createNotificationChannel(Context context) {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "LaundryChannel";
-            String description = "Channel to receive messages from the laundry machine";
+            CharSequence name = CHANNEL_NAME;
+            String description = CHANNEL_DESCRIPTION;
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
             // Register the channel with the system; you can't change the importance
             // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
-    }*/
+
+    }
 
 }
