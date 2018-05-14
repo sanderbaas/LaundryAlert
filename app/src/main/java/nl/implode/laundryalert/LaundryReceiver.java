d685c135b689144d91651bc40009ff2a9027bd12
Sander Baas <sander@implode.nl>
2018-05-14 17:21:07 +0200

    fixed passing of extra's in the intent to dismiss laundry operation

--- app/src/main/java/nl/implode/laundryalert/LaundryReceiver.java
+++ app/src/main/java/nl/implode/laundryalert/LaundryReceiver.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
@@ -29,6 +30,7 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 import static android.app.Notification.EXTRA_NOTIFICATION_ID;
+import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
 
 public class LaundryReceiver extends BroadcastReceiver {
     public static final String CHANNEL_ID = "nl.implode.laundryalert";
@@ -38,8 +40,26 @@ public class LaundryReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
+        if (intent.getAction() != null && intent.getAction() == "nl.implode.laundryalert.ACTION_DISMISS") {
+            Long timestampStart = null;
+            Integer messageId = null;
+            Bundle bundle = intent.getExtras();
+            timestampStart = bundle.getLong("timestampStart");
+            messageId = bundle.getInt("messageId");
+            dismissAlert(context, messageId, timestampStart);
+            return;
+        }
+        //Log.d("Intent", intent.toString());
         Log.d("Laundry", "I'm running");
         checkAndAlert(context);
+    }
+
+    public void dismissAlert(Context context, Integer messageId, Long timestampStart) {
+        Log.d("Laundry", "dismissing");
+        postHandler(timestampStart, "Sander");
+        //remove notification
+        NotificationManager notificationManager= (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(messageId);
     }
 
     public void checkAndAlert(Context context) {
@@ -53,11 +73,16 @@ public class LaundryReceiver extends BroadcastReceiver {
         String handledBy = null;
         Date handledTime = null;
         Integer messageId = null;
+        Long timestampDone = null;
+        Long timestampStart = null;
 
         try {
+            if (laundryStatus.has("timestamp_start") && !laundryStatus.isNull("timestamp_start")) {
+                timestampStart = laundryStatus.getLong("timestamp_start");
+            }
             if (laundryStatus.has("timestamp_done") && !laundryStatus.isNull("timestamp_done")) {
                 Log.d("Laundry", "is done");
-                Long timestampDone = laundryStatus.getLong("timestamp_done");
+                timestampDone = laundryStatus.getLong("timestamp_done");
                 Long longMessageId = timestampDone/1000L;
                 messageId = longMessageId.intValue();
                 Log.d("timestamp long", timestampDone.toString());
@@ -80,18 +105,18 @@ public class LaundryReceiver extends BroadcastReceiver {
         }
 
         if (done && !handled) {
-            title = "Laundry is done";
-            SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE d MMMM HH:mm");
-            message = "Done since: " + dateFormat.format(doneTime);
-            alertMessage(context, messageId, title, message);
+            title = "De was is klaar";
+            //SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE d MMMM HH:mm");
+            //message = "Done since: " + dateFormat.format(doneTime);
+            alertMessage(context, messageId, timestampStart, timestampDone, title);
         }
     }
 
-    public void postHandler() {
+    public void postHandler(Long timestampStart, String handler) {
         OkHttpClient client = new OkHttpClient();
         MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-        String url = "http://kelder.implode.nl:8124/handle";
-        String json = "{'handler': 'Sander'}";
+        String url = "http://kelder.implode.nl:8124/handle/" + timestampStart.toString();
+        String json = "{\"handler\": \"" + handler + "\"}";
         RequestBody body = RequestBody.create(JSON, json);
         Request request = new Request.Builder()
                 .url(url)
@@ -100,6 +125,7 @@ public class LaundryReceiver extends BroadcastReceiver {
         try {
             Response response = client.newCall(request).execute();
             Integer code = response.code();
+            Log.d("dismiss code", code.toString());
             response.body().string();
         } catch (Exception e) {
             return;
@@ -127,17 +153,29 @@ public class LaundryReceiver extends BroadcastReceiver {
         }
     }
 
-    public void alertMessage(Context context, Integer messageId, String title, String message) {
+    public void alertMessage(Context context, Integer messageId, Long timestampStart, Long timestampDone, String title) {
         createNotificationChannel(context);
+        Intent dismissIntent = new Intent(context, LaundryReceiver.class);
+        dismissIntent.setAction("nl.implode.laundryalert.ACTION_DISMISS");
+        dismissIntent.putExtra("messageId", messageId);
+        dismissIntent.putExtra("timestampStart", timestampStart);
+        Log.d("alert timestampStart", timestampStart.toString());
+        PendingIntent dismissPendingIntent =
+                PendingIntent.getBroadcast(context, 0, dismissIntent, FLAG_UPDATE_CURRENT);
+        NotificationCompat.Action dismissAction = new NotificationCompat.Action.Builder(
+                android.R.drawable.btn_default,
+                context.getString(R.string.dismiss),
+                dismissPendingIntent).build();
+
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setWhen(timestampDone)
                 .setSmallIcon(R.drawable.wash)
                 .setContentTitle(title)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .addAction(dismissAction);
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         Integer notificationId = messageId;
         notificationManager.notify(notificationId, mBuilder.build());
-
     }
 
     private void createNotificationChannel(Context context) {
