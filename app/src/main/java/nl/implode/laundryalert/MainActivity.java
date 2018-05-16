c0bad8b01917b4138dfb189957efe47a0d9c04aa
Sander Baas <sander@implode.nl>
2018-05-16 17:07:32 +0200

    kind of working discovery of server
    it only works when also sending a broadcast first

--- app/src/main/java/nl/implode/laundryalert/MainActivity.java
+++ app/src/main/java/nl/implode/laundryalert/MainActivity.java
@@ -5,29 +5,34 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
+
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+
 import android.os.Handler;
-import android.os.Message;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
 import java.net.InetAddress;
-import java.net.MulticastSocket;
 import java.net.UnknownHostException;
-import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
     private PendingIntent pendingIntent;
+    //private static final String MDNS_LAUNDRY_SERVICENAME = "laundry._http._tcp.local.";
+    //private static final String MDNS_LAUNDRY_SERVICENAME = "_http._tcp.local";
+    //private static final int DISCOVERY_TIMEOUT = 5000;
     //private PendingIntent alarmIntent;
 
     /*Handler handler = new Handler() {
@@ -67,6 +72,35 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public static InetAddress intToInetAddress(int hostAddress) {
+        if (hostAddress == 0)
+            return null;
+
+        byte[] addressBytes = { (byte)(0xff & hostAddress),
+                (byte)(0xff & (hostAddress >> 8)),
+                (byte)(0xff & (hostAddress >> 16)),
+                (byte)(0xff & (hostAddress >> 24)) };
+
+        try {
+            return InetAddress.getByAddress(addressBytes);
+        } catch (UnknownHostException e) {
+            throw new AssertionError();
+        }
+    }
+
+    public void noHostFound() {
+        Log.d("Laundry", "no host found");
+    }
+
+    public void foundHosts(final ServiceInfo[] serviceInfos) {
+        for (Integer i = 0; i < serviceInfos.length; i++ ) {
+            String name = serviceInfos[i].getName();
+            Integer port = serviceInfos[i].getPort();
+            String host = serviceInfos[i].getServer();
+            Log.d("Found host", name + ": " + host + ":" + port.toString());
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -102,6 +136,111 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
                 MainActivity.this.startActivity(settingsIntent);
+            }
+        });
+
+        /*class SampleListener implements ServiceListener {
+            @Override
+            public void serviceAdded(ServiceEvent event) {
+                System.out.println("Service added: " + event.getInfo());
+            }
+
+            @Override
+            public void serviceRemoved(ServiceEvent event) {
+                System.out.println("Service removed: " + event.getInfo());
+            }
+
+            @Override
+            public void serviceResolved(ServiceEvent event) {
+                System.out.println("Service resolved: " + event.getInfo());
+            }
+        }*/
+
+        Button discover = (Button) findViewById(R.id.discover);
+        discover.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Log.d("Laundry", "click discover");
+
+                /*try {
+                    // Create a JmDNS instance
+                    WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+                    WifiManager.MulticastLock multicastLock = null;
+                    int wifiIpAddress = wifiManager.getConnectionInfo().getIpAddress();
+                    InetAddress wifiInetAddress = intToInetAddress(wifiIpAddress);
+                    JmDNS jmdns = JmDNS.create(wifiInetAddress);
+
+                    // Add a service listener
+                    jmdns.addServiceListener("_xbmc-jsonrpc-h._tcp.local.", new SampleListener());
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }*/
+
+                final Handler handler = new Handler();
+                final Thread searchThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("Laundry", "searchThread running");
+                        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+                        WifiManager.MulticastLock multicastLock = null;
+                        try {
+                            // Get wifi ip address
+                            int wifiIpAddress = wifiManager.getConnectionInfo().getIpAddress();
+                            InetAddress wifiInetAddress = intToInetAddress(wifiIpAddress);
+
+                            // Acquire multicast lock
+                            multicastLock = wifiManager.createMulticastLock("laundryalert.multicastlock");
+                            multicastLock.setReferenceCounted(true);
+                            multicastLock.acquire();
+
+                            /*JmDNS jmDns = (wifiInetAddress != null)?
+                                    JmDNS.create(wifiInetAddress) :
+                                    JmDNS.create();*/
+                            //JmDNS jmDns = JmDNS.create(wifiInetAddress);
+                            JmDNS jmDns = JmDNS.create(wifiInetAddress);
+
+                            ServiceInfo serviceInfo = ServiceInfo.create("http", "example", 1234, "path=index.html");
+                            jmDns.registerService(serviceInfo);
+                            //_laundry._http._tcp.local._http._tcp.local
+                            // Get the json rpc service list
+                            final ServiceInfo[] serviceInfos =
+                                    jmDns.list("laundry._http._tcp.local.", 10000);
+
+                            Log.d("Laundry", String.valueOf(serviceInfos.length));
+
+                            //synchronized (lock) {
+                            //    // If the user didn't cancel the search, and we are sill in the activity
+                            //    if (!searchCancelled && isAdded()) {
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Log.d("Laundry", "running handler");
+                                            if ((serviceInfos == null) || (serviceInfos.length == 0)) {
+                                                Log.d("Laundry", "no hosts found");
+                                                noHostFound();
+                                            } else {
+                                                Log.d("Laundry", "found hosts");
+                                                foundHosts(serviceInfos);
+                                            }
+                                        }
+                                    });
+                            //    }
+                            //}
+                        } catch (IOException e) {
+                            Log.d("Laundry", "Got an IO Exception", e);
+                        } finally {
+                            if (multicastLock != null)
+                                multicastLock.release();
+                        }
+                    }
+                });
+
+                searchThread.start();
+
+                //Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+                //MainActivity.this.startActivity(settingsIntent);
             }
         });
 
--- app/src/main/java/nl/implode/laundryalert/SettingsActivity.java
+++ app/src/main/java/nl/implode/laundryalert/SettingsActivity.java
@@ -237,6 +237,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
+            bindPreferenceSummaryToValue(findPreference("server_address"));
             bindPreferenceSummaryToValue(findPreference("sync_frequency"));
             bindPreferenceSummaryToValue(findPreference("nag_frequency"));
         }
